8e393ba6fbe9d35449314bbd578338fd1ecfc089
dexia2 <lindan.k2012.af.nk@gmail.com>
2018-09-13 10:58:39 +0900

    add scss style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pf-root',
   templateUrl: './app.component.html',
-  styles: []
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Portfolio2018';
